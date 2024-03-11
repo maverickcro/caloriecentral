5e4bba6e13e0a94878d12ec841fbb402437a19c5
Marko <mav3ricktv@gmail.com>
2024-03-11 21:49:29 +0100

    New toggle animation + light/dark theme is set according to local time, 7-20h is light theme

--- app/components/ToggleMode.tsx
+++ app/components/ToggleMode.tsx
@@ -1,29 +1,40 @@
 "use client";
 import { useEffect, useState } from "react";
-import { FaSun, FaMoon } from "react-icons/fa";
 import { useTheme } from "next-themes";
 
 export default function ToggleMode() {
   const [mounted, setMounted] = useState(false);
+
   const { theme, setTheme } = useTheme();
 
+  useEffect(() => {
+    const hours = new Date().getHours();
+    const newTheme = hours > 7 && hours < 20 ? "light" : "dark";
+    setTheme(newTheme);
+  }, [setTheme]);
+
+  // Ensure correct theme is set on mount
   useEffect(() => {
     setMounted(true);
   }, []);
 
-  if (!mounted) {
-    return null;
-  }
+  if (!mounted) return null;
+
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+  console.log(theme);
   return (
-    <button
-      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-      className="flex items-center justify-center rounded-md focus:outline-none"
-    >
-      {theme === "light" ? (
-        <FaSun className="text-black text-xl" />
-      ) : (
-        <FaMoon className="text-white text-xl" />
-      )}
-    </button>
+    <div className="toggle-container">
+      <input
+        id="toggle"
+        className="toggle"
+        type="checkbox"
+        onChange={toggleTheme}
+        checked={theme === "dark"}
+        // aria-label for accessibility
+        aria-label="Toggle dark mode"
+      />
+    </div>
   );
 }
