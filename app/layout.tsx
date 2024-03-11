a7b8bcf019f90d26912e02a9778e7e0c881f0b21
Marko <mav3ricktv@gmail.com>
2024-03-11 20:46:10 +0100

    dark mode finished, latin font removed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,7 @@
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import { Inter } from "next/font/google";
 import { ThemeProvider } from "./theme-provider";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: {
@@ -24,7 +21,6 @@ export default function RootLayout({
       <body
         suppressHydrationWarning={true}
         className={`
-        ${inter.className}
          bg-gray-100 dark:bg-slate-900 duration-200`}
         style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
       >
