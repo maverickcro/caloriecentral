b34c907783ea325010a0f067a6565e1015702a1b
maverickcro <mav3ricktv@gmail.com>
2024-03-13 23:14:19 +0100

    speed insights vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { ThemeProvider } from "./theme-provider";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata = {
   title: {
