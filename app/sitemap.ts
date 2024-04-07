4e1a74a34648c965f66b4c76b462eff8a0d57e94
Marko <mav3ricktv@gmail.com>
2024-04-07 19:37:59 +0200

    sitemap changed

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,102 +1,54 @@
-import { MetadataRoute } from 'next'
- 
+import { MetadataRoute } from "next";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://calorie-pal.com',
+      url: "https://calorie-pal.com",
       lastModified: new Date(),
-      changeFrequency: 'yearly',
-      priority: 1,
     },
     {
-      url: 'https://calorie-pal.com/about',
+      url: "https://calorie-pal.com/calculators",
       lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
     },
     {
-      url: 'https://calorie-pal.com/blog',
+      url: "https://calorie-pal.com/carbs-calculator",
       lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.5,
     },
     {
-      url: 'https://calorie-pal.com/calculators',
+      url: "https://calorie-pal.com/protein-calculator",
       lastModified: new Date(),
-      changeFrequency: 'daily',
-      priority: 0.7,
     },
     {
-      url: 'https://calorie-pal.com/disclaimer',
+      url: "https://calorie-pal.com/sugar-calculator",
       lastModified: new Date(),
-      changeFrequency: 'daily',
-      priority: 0.7,
     },
     {
-      url: 'https://calorie-pal.com/carbs-calculator',
+      url: "https://calorie-pal.com/subway-calculator",
       lastModified: new Date(),
-      changeFrequency: 'daily',
-      priority: 0.7,
     },
     {
-      url: 'https://calorie-pal.com/protein-calculator',
+      url: "https://calorie-pal.com/tdee-calculator",
       lastModified: new Date(),
-      changeFrequency: 'daily',
-      priority: 0.7,
     },
     {
-      url: 'https://calorie-pal.com/sugar-calculator',
+      url: "https://calorie-pal.com/calorie-deficit-calculator",
       lastModified: new Date(),
-      changeFrequency: 'daily',
-      priority: 0.7,
     },
     {
-      url: 'https://calorie-pal.com/blog',
+      url: "https://calorie-pal.com/fasting-calculator",
       lastModified: new Date(),
-      changeFrequency: 'daily',
-      priority: 0.7,
     },
     {
-      url: 'https://calorie-pal.com/subway-calculator',
+      url: "https://calorie-pal.com/body-recomposition-calculator",
       lastModified: new Date(),
-      changeFrequency: 'daily',
-      priority: 0.7,
     },
     {
-      url: 'https://calorie-pal.com/tdee-calculator',
+      url: "https://calorie-pal.com/fat-calculator",
       lastModified: new Date(),
-      changeFrequency: 'daily',
-      priority: 0.7,
     },
     {
-      url: 'https://calorie-pal.com/calorie-deficit-calculator',
+      url: "https://calorie-pal.com/anorexic-calculator",
       lastModified: new Date(),
-      changeFrequency: 'daily',
-      priority: 0.7,
     },
-    {
-      url: 'https://calorie-pal.com/fasting-calculator',
-      lastModified: new Date(),
-      changeFrequency: 'daily',
-      priority: 0.7,
-    },
-    {
-      url: 'https://calorie-pal.com/body-recomposition-calculator',
-      lastModified: new Date(),
-      changeFrequency: 'daily',
-      priority: 0.7,
-    },
-    {
-      url: 'https://calorie-pal.com/fat-calculator',
-      lastModified: new Date(),
-      changeFrequency: 'daily',
-      priority: 0.7,
-    },
-    {
-      url: 'https://calorie-pal.com/anorexic-calculator',
-      lastModified: new Date(),
-      changeFrequency: 'daily',
-      priority: 0.7,
-    }
-]
+  ];
 }
